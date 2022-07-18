c17d1444a2181b60edfbce107493d61f3c86a541
MKW420 <keitumetseshongwe@gmail.com>
2022-07-18 16:07:45 +0200

    code added

--- js/main.js
+++ js/main.js
@@ -12,20 +12,28 @@
 
 //using selectors inside the element
 const questions = document.querySelectorAll('.question');
+const section = document.getElementById('ctnr');
 
 questions.forEach(function (question){
     //console.log(orange)
     const btn = question.querySelector('.question-btn');
    // console.log(btn);
    btn.addEventListener('click', function(){
+    section.addEventListener('click', function(e){
+        section.style.height = "250px";
+       
+         
+    })
     questions.forEach(function(item){
            //console.log(item)
+           
            if(item !== question){
             item.classList.remove('show-text')
            }
     })
-
+ 
     question.classList.toggle('show-text')
+
    })
 })
 
